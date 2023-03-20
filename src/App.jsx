14f0f5e932a6e4ae6eb4a23a2ac1d7ff436b8c47
Freshta Ebrahim <freshtaebrahim@hotmail.co.uk>
2023-03-20 21:06:30 +0000

    use flexbox for cards, introduce images and title props

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,22 @@ function App() {
     <div className="App">
       <h1>Landmarks</h1>
       <h2>Click on the card to learn more about the landmark</h2>
-      <Card frontSide="London" backSide="Buckingham Palace"/>
-      <Card frontSide="Manchester" backSide="Old Trafford"/>
-      <Card frontSide="Liverpool" backSide="Royal Albert Hall"/>
-      <Card frontSide="Bath" backSide="Roman Baths"/>
-      <Card frontSide="Cornwall" backSide="Eden Project"/>
-      <Card frontSide="Wales" backSide="Snowdonia National Park"/>
+
+      <div className='parent-card-div'>
+
+      <div className='child-card-div'>
+      <Card frontSideTitle="London"  backSideTitle="Buckingham Palace" frontSideImage={"https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTYyNDg1MjE3MTI1Mjc5Mzk4/topic-london-gettyimages-760251843-promo.jpg"} backSideImage={""} />
+      <Card frontSideTitle="Manchester" backSideTitle="Old Trafford" frontSideImage={""} backSideImage={""}/>
+      <Card frontSideTitle="Liverpool" backSideTitle="Royal Albert Hall" frontSideImage={""} backSideImage={""}/>
+      </div>
+
+      <div className='child-card-div'>
+      <Card frontSideTitle="Bath" backSideTitle="Roman Baths" frontSideImage={""} backSideImage={""}/>
+      <Card frontSideTitle="Cornwall" backSideTitle="Eden Project" frontSideImage={""} backSideImage={""}/>
+      <Card frontSideTitle="Wales" backSideTitle="Snowdonia National Park" frontSideImage={""} backSideImage={""}/>
+      </div>
+
+      </div>
 
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from "react"
 import "./Card.css"
 
-export default function Card({ frontSide, backSide }) {
+export default function Card({ frontSideTitle, backSideTitle, frontSideImage, backSideImage }) {
   const [isFront, changeFace] = useState(true);
+
   function handleClick() {
     changeFace(oldState => !oldState);
   }
-  const text = isFront ? frontSide : backSide;
+
+  const text = isFront ? frontSideTitle : backSideTitle;
   const sideClass = isFront ? "front" : "back";
   const classList = `flash-card ${sideClass}`
+
+  //Pass down image props from App.jsx
+  //Create props for backSideText to display on back of cards
+  //Adjust CSS for images etc 
+  
   return (
     <div className={classList} onClick={handleClick}>
       {text}
+      <img src={isFront ? frontSideImage : backSideImage} width="200px"/>
+
+      {!isFront ? <p>Example text here</p> : null}
     </div>
   );
 }
